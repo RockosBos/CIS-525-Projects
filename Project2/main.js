f402edaa73e96a243f6be5d17afb7b617eb0628d
RockosBos <32206608+RockosBos@users.noreply.github.com>
2024-10-19 23:52:53 -0400

    Working State

--- Project2/main.js
+++ Project2/main.js
@@ -6,55 +6,16 @@ const backImgSrc = "images/back.png";
 const blankImgSrc = "images/blank.png";
 const cardImgSrcStart = "images/card_";
 
+let score = 0;
+let cardsSelected = 0;
+let previousCards = [];
+
+let name;
+let highScore = 0;
+let correct = 0;
+
 let cards = [
-	{id: 1, shown: false},
-	{id: 2, shown: false},
-	{id: 3, shown: false},
-	{id: 4, shown: false},
-	{id: 5, shown: false},
-	{id: 6, shown: false},
-	{id: 7, shown: false},
-	{id: 8, shown: false},
-	{id: 9, shown: false},
-	{id: 10, shown: false},
-	{id: 11, shown: false},
-	{id: 12, shown: false},
-	{id: 13, shown: false},
-	{id: 14, shown: false},
-	{id: 15, shown: false},
-	{id: 16, shown: false},
-	{id: 17, shown: false},
-	{id: 18, shown: false},
-	{id: 19, shown: false},
-	{id: 20, shown: false},
-	{id: 21, shown: false},
-	{id: 22, shown: false},
-	{id: 23, shown: false},
-	{id: 24, shown: false},
-	{id: 25, shown: false},
-	{id: 26, shown: false},
-	{id: 27, shown: false},
-	{id: 28, shown: false},
-	{id: 29, shown: false},
-	{id: 30, shown: false},
-	{id: 31, shown: false},
-	{id: 32, shown: false},
-	{id: 33, shown: false},
-	{id: 34, shown: false},
-	{id: 35, shown: false},
-	{id: 36, shown: false},
-	{id: 37, shown: false},
-	{id: 38, shown: false},
-	{id: 39, shown: false},
-	{id: 40, shown: false},
-	{id: 41, shown: false},
-	{id: 42, shown: false},
-	{id: 43, shown: false},
-	{id: 44, shown: false},
-	{id: 45, shown: false},
-	{id: 46, shown: false},
-	{id: 47, shown: false},
-	{id: 48, shown: false},
+	
 
 ]
 
@@ -62,34 +23,38 @@ let card = "<a href='#'><img src='./images/back.png' /></a>";
 
 let cardRow = [];
 
-
-document.addEventListener("DOMContentLoaded", () => {
-    // display cards and player info
-    
-
-    // load settings data
-    
- 
-	// add click event handler for each card link
-
-
-    // add click event handler for each tab link button
-
-    
-
-    // add click event handler for Save Settings button
-    
-    
-}); 
+let numCards;
 
 window.onload = function(){
 	//localStorage.setItem("numCards", 6);
 	console.log("Card Rows: " + localStorage.numCards / 8);
 	newGame();
+
+	document.getElementById("player").innerText = localStorage.name;
+	document.getElementById("player_name").value = localStorage.name;
+	document.getElementById("num_cards").value = localStorage.numCards;
+	document.getElementById("high_score").innerHTML = localStorage.highScore;
+	document.getElementById("correct").innerHTML = localStorage.correct;
 }
 
 function newGame(){
-	addRow(localStorage.getItem("numCards") / 8);
+	numCards = localStorage.getItem("numCards")
+	for(let i = 0; i < numCards / 2; i++){
+		cards.push({id: i + 1, shown: false});
+	}
+	for(let j = 0; j < numCards / 2; j++){
+		cards.push({id: j + 24 + 1, shown: false});
+	}
+	let index = cards.length;
+	console.log(cards);
+	for (var i = cards.length - 1; i >= 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+	
+	addRow(numCards / 8);
 }
 
 function setActive(tab){
@@ -120,27 +85,68 @@ function saveSettings(){
 
 	localStorage.setItem("name", name);
 	localStorage.setItem("numCards", numCards);
+
+	document.getElementById("player").innerText = name;
 	alert("Settings Saved Successfully");
 }
 
 function createRow(rowNum){
 	for(let i = 0; i < 8; i++){
-		let cardNum = (i + 1 + (rowNum * 8)).toString()
-		card = "<a id='" + cardNum + "' href='#' onclick='flipCard(" + cardNum + ")'><img src='./images/back.png' /></a>"
+		let cardNum = cards[(i + 1 + (rowNum * 8)) - 1].id;
+		card = "<a id='" + cardNum + "' href='#' onclick='userSelectCard(" + cardNum + ")'><img src='./images/back.png' /></a>"
 		cardRow.push(card);
 	}
 
 }
 
+function userSelectCard(cardNum){
+	if(!cards.find(({id}) => id === cardNum).shown){
+		if(cardsSelected <= 1){
+			setInterval(flipCard(cardNum), 2500);
+			cardsSelected++;
+			previousCards.push(cardNum);
+		}
+		else{
+			setInterval(flipCard(previousCards[0]), 2500);
+			setInterval(flipCard(previousCards[1]), 2500);
+			previousCards = [];
+			cardsSelected = 0;
+		}
+		if(cardsSelected == 2 && previousCards[0] % 24 == previousCards[1] % 24){
+			previousCards = [];
+			cardsSelected = 0;
+			correct = correct + 2;
+		}
+		localStorage.setItem("correct", correct);
+		if(correct > localStorage.highScore){
+			highScore = correct;
+			localStorage.setItem("highScore", highScore);
+		}
+		console.log("Correct: " + correct + " High: " + highScore);
+		document.getElementById("high_score").innerHTML = (localStorage.highScore).toString();
+		document.getElementById("correct").innerHTML = (localStorage.correct).toString();
+	}
+}
+
+
 function flipCard(cardID){
-	if(cards[cardID - 1].shown){
-		cards[cardID - 1].shown = false;
-		document.getElementById(cardID).children[0].setAttribute("src", "./images/back.png");
-	}
-	else{
-		cards[cardID - 1].shown = true;
-		document.getElementById(cardID).children[0].setAttribute("src", "./images/card_" + ((cardID % 24) + 1) + ".png");
-	}
+		console.log(cards.find(({id}) => id === cardID));
+		if(cards.find(({id}) => id === cardID).shown){
+			cards.find(({id}) => id === cardID).shown = false;
+			document.getElementById(cardID).children[0].setAttribute("style", "opacity: 0; transition: opacity 0.5s;");
+			setTimeout(function(){
+				document.getElementById(cardID).children[0].setAttribute("src", "./images/back.png");
+				document.getElementById(cardID).children[0].setAttribute("style", "opacity: 1; transition: opacity 0.5s;");
+			}, 250);
+		}
+		else{
+			cards.find(({id}) => id === cardID).shown = true;
+			document.getElementById(cardID).children[0].setAttribute("style", "opacity: 0; transition: opacity 0.5s;");
+			setTimeout(function(){
+				document.getElementById(cardID).children[0].setAttribute("src", "./images/card_" + ((cardID % 24) + 1) + ".png");
+				document.getElementById(cardID).children[0].setAttribute("style", "opacity: 1; transition: opacity 0.5s;");
+			}, 250);
+		}
 }
 
 function clearRow(){

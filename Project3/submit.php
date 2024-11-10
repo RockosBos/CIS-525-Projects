7d6ca3d91b07a65b6bfb1f602ea8e94c59d3f30e
RockosBos <32206608+RockosBos@users.noreply.github.com>
2024-11-09 22:20:26 -0500

    php changes

--- Project3/submit.php
+++ Project3/submit.php
@@ -1,9 +1,54 @@
-<?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$UMID = $_POST["name"];
-	$FIRSTNAME = $_POST["FirstName"];
-	$LASTNAME = $_POST["LastName"]
-}
+<!DOCTYPE html>
+<html>
 
+<head>
+    <title>Insert Page page</title>
+</head>
 
-?>
+<body>
+    <center>
+        <?php
+
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "staff");
+        
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+        
+        // Taking all 5 values from the form data(input)
+		$umid = $_REQUEST['umid'];
+        $first_name =  $_REQUEST['first_name'];
+        $last_name = $_REQUEST['last_name'];
+        $email =  $_REQUEST['gender'];
+        $phonenumber = $_REQUEST['address'];
+        
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO college  VALUES ('$first_name', 
+            '$last_name','$gender','$address','$email')";
+        
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully." 
+                . " Please browse your localhost php my admin" 
+                . " to view the updated data</h3>"; 
+
+            echo nl2br("\n$first_name\n $last_name\n "
+                . "$gender\n $address\n $email");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+        
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+
+</html>
